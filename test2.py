dc02b66cb55bff7ad3b465d0b9e11168a7a8d747
kailashs2002 <kailashseshadri2002@gmail.com>
2023-01-08 04:52:36 -0700

    More tests (yay) + tts generator

--- test2.py
+++ test2.py
@@ -4,18 +4,18 @@ from latex2sympy2 import latex2sympyStr
 
 def main():
     testList = [
-        r"x^1",
-        r"x^2",
-        r"x^3",
-        r"x^4",
-        r"x^{-1}",
-        r"x^{-2}",
-        r"e^1",
-        r"e^2",
-        r"e^3",
-        r"e^4",
-        r"e^{-1}",
-        r"e^{-2}",
+        # r"x^1",
+        # r"x^2",
+        # r"x^3",
+        # r"x^4",
+        # r"x^{-1}",
+        # r"x^{-2}",
+        # r"e^1",
+        # r"e^2",
+        # r"e^3",
+        # r"e^4",
+        # r"e^{-1}",
+        # r"e^{-2}",
 
         "---",
         
@@ -45,6 +45,7 @@ def main():
         r"2 4 y",
 
 
+        
 
 
     ]
