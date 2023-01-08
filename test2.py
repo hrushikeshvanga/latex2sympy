c4d0d210a6c005c1a869d8a8a515c2aa8d78b1ba
kailashs2002 <kailashseshadri2002@gmail.com>
2023-01-08 04:31:20 -0700

    uncommented test cases

--- test2.py
+++ test2.py
@@ -4,40 +4,47 @@ from latex2sympy2 import latex2sympyStr
 
 def main():
     testList = [
-        # r"x^1",
-        # r"x^2",
-        # r"x^3",
-        # r"x^4",
-        # r"x^{-1}",
-        # r"x^{-2}",
-        # r"e^1",
-        # r"e^2",
-        # r"e^3",
-        # r"e^4",
-        # r"e^{-1}",
-        # r"e^{-2}",
+        r"x^1",
+        r"x^2",
+        r"x^3",
+        r"x^4",
+        r"x^{-1}",
+        r"x^{-2}",
+        r"e^1",
+        r"e^2",
+        r"e^3",
+        r"e^4",
+        r"e^{-1}",
+        r"e^{-2}",
 
         "---",
-
+        
+        #Derivatives tests
         r"\frac{\partial f(x,y)}{\partial x} ",
         r"\frac{df(x)}{dx} ",
         r"\frac{\partial}{\partial x} f(x,y) ",
         r"\frac{d}{dx} f(x)",
+        r"\frac{\partial}{\partial x} \frac{1}{xy} ",
+        r"\frac{d}{dx}  \frac{1}{xy}",
 
-        # r"f(x)",
-        # r"f(x+1)",
-        # r"f(x^2)",
-        # r"\sqrt{\frac{1}{x}}",
-        # r"\sum_{n = 0}^{\infty} \frac{1}{n!}",
-        # r"\prod^c_{a = b} x^{a!}",
-        # r"\log{e^{2*x!}}"
+        # Functions tests
+        r"f(x)",
+        r"f(x+1)",
+        r"f(x^2)",
+        r"\sqrt{\frac{1}{x}}",
+        r"\sum_{n = 0}^{\infty} \frac{1}{n!}",
+        r"\prod^c_{a = b} x^{a!}",
+        r"\log{e^{2*x!}}"
 
+        # Integer * Symbol tests
         r"2x2y",
         r"2x2",
         r"2 4 2y",
         r"24 y",
         r"2 4 y",
         r"2 4 y",
+
+
 
 
     ]
