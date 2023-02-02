b7e2b2fc4f95fbe1bb113b07b794b6b5d9baccbf
Joshua Ji <joshua.ji@jiwhiz.com>
2023-02-02 04:56:31 +0000

    Gitignore service account key

--- tts.py
+++ tts.py
@@ -7,7 +7,8 @@ Note: ssml must be well-formed according to:
 from google.cloud import texttospeech
 
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="theta-carving-374101-751fbd97e1f0.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service_account_key.json"
+
 
 def generate_mp3():
     # Instantiates a client
@@ -18,23 +19,23 @@ def generate_mp3():
     words = file.read()
     file.close()
     print(words)
-    synthesis_input = texttospeech.SynthesisInput(text = words)
+    synthesis_input = texttospeech.SynthesisInput(text=words)
 
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
     voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", name="en-US-Neural2-D"
+        language_code="en-US", name="en-US-Neural2-D"
     )
 
     # Select the type of audio file you want returned
     audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
+        audio_encoding=texttospeech.AudioEncoding.MP3
     )
 
     # Perform the text-to-speech request on the text input with the selected
     # voice parameters and audio file type
     response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
+        input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
     # The response's audio_content is binary.
